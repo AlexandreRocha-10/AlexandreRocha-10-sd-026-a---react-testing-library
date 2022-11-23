a951f0e1f50176ea45becdda847572db905480cf
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-11-23 18:46:30 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/RenderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const noFav = screen.getByText(/No favorite Pokémon found/i);
+
+    expect(noFav).toBeInTheDocument();
+  });
+});
